c7989ab169b76f2d25fd8d1ba84265d60cd588e1
satishkhatri <satish.khatri80@gmail.com>
2017-12-09 15:04:48 +0000

    add button and ordered list class

---
+++ button.php
@@ -0,0 +1,40 @@
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="./include/css/bootstrap.css" type="text/css"/>
+    <link rel="stylesheet" href="./include/css/style.css" type="text/css"/>
+</head>
+<body>
+<div class="bgimage">
+    <div class="container">
+        <h1>Satish Khatri</h1>
+    </div>
+    <div class="raw">
+        <section class="col-md-6">
+            <a class="btn btn-default" href="https://www.google.co.uk/" role="button">link</a>
+            <input class="btn btn-default" name="submit" value="submit"/>
+            <button class="btn btn-sm" type="submit">small</button>
+            <button class="btn btn-lg" type="submit">large</button>
+            <button class="btn btn-link" type="submit">link</button>
+            <button class="btn btn-xs" type="submit">xtra small</button>
+        </section>
+        <section class="col-md-6">
+            <button class="btn btn-default" type="submit">default</button>
+            <button class="btn btn-primary" type="submit">primary</button>
+            <button class="btn btn-success" type="submit">success</button>
+            <button class="btn btn-warning" type="submit">warning</button>
+            <button class="btn btn-danger" type="submit">Danger</button>
+
+
+        </section>
+    </div>
+</div>
+<div class="services container" style="padding-top: 10px">
+
+
+</div>
+<script src="include/js/jquery-3.2.1.js"></script>
+<script src="include/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ ordered-list.php
@@ -0,0 +1,43 @@
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="./include/css/bootstrap.css" type="text/css"/>
+    <link rel="stylesheet" href="./include/css/style.css" type="text/css"/>
+</head>
+<body>
+<div class="bgimage">
+    <div class="container">
+        <h1>Satish Khatri</h1>
+    </div>
+    <div class="raw">
+
+    </div>
+</div>
+<div class="services container" style="padding-top: 10px">
+
+    <div class="raw">
+        <section class="col-md-6">
+            <ol class="list-inline">
+                <li>Cheese</li>
+                <li>Egg</li>
+                <li>Bread</li>
+                <li>
+                    <ul>
+                        <li>Satish</li>
+                        <li>Khatr</li>
+                    </ul>
+                </li>
+            </ol>
+        </section>
+        <section class="col-md-6">
+            <a class="btn btn-default" href="https://www.google.co.uk/" role="button">link</a>
+            <input class="btn btn-default" name="submit" value="submit"/>
+            <button class="btn btn-default" type="submit">Hello</button>
+        </section>
+
+</div>
+<script src="include/js/jquery-3.2.1.js"></script>
+<script src="include/js/bootstrap.js"></script>
+</body>
+</html>
