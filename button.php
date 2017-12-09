edaa40ee7d3c448dd60ac1feed94f3d2fbadc38d
satishkhatri <satish.khatri80@gmail.com>
2017-12-09 16:29:13 +0000

    data amended

--- button.php
+++ button.php
@@ -10,8 +10,10 @@
     <div class="container">
         <h1>Satish Khatri</h1>
     </div>
-    <div class="raw">
-        <section class="col-md-6">
+</div>
+    <div class="raw text-center">
+        <section class="col-lg-12">
+            <div class="h1">Buttton sizes</div>
             <a class="btn btn-default" href="https://www.google.co.uk/" role="button">link</a>
             <input class="btn btn-default" name="submit" value="submit"/>
             <button class="btn btn-sm" type="submit">small</button>
@@ -19,13 +21,17 @@
             <button class="btn btn-link" type="submit">link</button>
             <button class="btn btn-xs" type="submit">xtra small</button>
         </section>
-        <section class="col-md-6">
+        <section class="col-lg-12">
+            <div class="h1">Different types of buttons</div>
             <button class="btn btn-default" type="submit">default</button>
             <button class="btn btn-primary" type="submit">primary</button>
             <button class="btn btn-success" type="submit">success</button>
             <button class="btn btn-warning" type="submit">warning</button>
             <button class="btn btn-danger" type="submit">Danger</button>
-
+        </section>
+        <section class="col-lg-12">
+            <div class="h1"> Button enable /disable</div>
+            <button class="btn btn-active" type="submit">active</button>
 
         </section>
     </div>
--- index.php
+++ index.php
@@ -1,7 +1,54 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Satish Khatri
- * Date: 08/12/2017
- * Time: 10:20 AM
- */
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="./include/css/bootstrap.css" type="text/css"/>
+    <link rel="stylesheet" href="./include/css/style.css" type="text/css"/>
+</head>
+<body>
+<div class="bgimage">
+    <div class="container">
+        <h1>Satish Khatri</h1>
+    </div>
+    <div class="raw">
+
+    </div>
+</div>
+<div class="services container" style="padding-top: 10px">
+    <div class="raw">
+        <section class="col-xs-12">
+            <p class="text-left text-capitalize">compressed file savescompressed file savescompressed file saves3fdgdfgdfg<br>dfgdfgdfg<br> dgd dg dg df s of jQuery files  bandwidth and improves performance in production. You can also download a sourcemap file for use when debugging with a compressed file</p>
+            <p>Compressed the compressed file saves bandwidth and improves performan</p>
+            <blockquote>
+                <p>Compressed the compressedsaves bandwidth and improves performan saves bandwidth and improves performan saves bandwidth and improves performan file saves bandwidth and improves performan
+                </p>
+                <footer>Satish Khatri</footer>
+            </blockquote>
+        </section>
+
+    </div>
+    <div class="raw">
+        <section class="col-md-6">
+            <ol class="list-inline">
+                <li>Cheese</li>
+                <li>Egg</li>
+                <li>Bread</li>
+                <li>
+                    <ul>
+                        <li>Satish</li>
+                        <li>Khatr</li>
+                    </ul>
+                </li>
+            </ol>
+        </section>
+        <section class="col-md-6">
+            <a class="btn btn-default" href="https://www.google.co.uk/" role="button">link</a>
+            <input class="btn btn-default" name="submit" value="submit"/>
+            <button class="btn btn-default" type="submit">Hello</button>
+        </section>
+    </div>
+</div>
+<script src="include/js/jquery-3.2.1.js"></script>
+<script src="include/js/bootstrap.js"></script>
+</body>
+</html>
